4e021587a14bf8f2ab27dba0cc6d1ed03a4ebef0
ofersabo <saboof@post.bgu.ac.il>
2018-11-12 14:40:10 +0200

    two backwards 93.4 can achieve 94 if removes handling special case

--- HMMTag.py
+++ HMMTag.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys
 lambda_interpolation = [0.8,0.15,0.05]
 all_states = set()
+special_tags = set([".","$","``",'(',')',"''",',',':','#','``'])
+tags_to_go_over = all_states - special_tags
 # word_to_state_probability = {}
 
 from collections import Counter
@@ -13,6 +15,8 @@ deno = 'denominator'
 
 
 def create_denominator():
+    global tags_to_go_over
+    tags_to_go_over = all_states - special_tags
     for d in [trigram,bigram,emission]:
         for couple in d:
             all_value = d[couple].values()
@@ -155,63 +159,77 @@ def synthesize_sentence(words,tags_list):
 
 
 def is_special_case(trellis,word,i,words, prev_tag,prev_prev_tag):
+    two_backwords_is_known = False
     if word == "." and i == len(words)-1:
         probability, prev_tag_at_state = max([(trellis[-2][prev_tag][1] , prev_tag) for prev_tag in all_states])
         trellis[-1][word] = (prev_tag_at_state,0)
         return True
-
-    if unigram[word] < 1000:
-        if not word in emission:
-            return False
-        elif not tag in emission[word]:
-            return handle_no_tag_per_word()
-
-        emision_probability(word, tag)
+    if word in special_tags or word.replace('.','',1).replace(',',' ').isdigit():
+        if word.replace('.','',1).replace(',',' ').isdigit():
+            word = "CD"
+        if len(trellis) == 1:    # only first word is like that
+            sure_prev_tag = prev_tag
+            trellis[-1][word] = (prev_tag, 0)
+            return True
+        if len(trellis[-2].keys()) > 1:
+            probability, prev_tag_at_state = max([(trellis[-2][prev_tag][1], prev_tag) for prev_tag in tags_to_go_over])
+            trellis[-1][word] = (prev_tag_at_state, 0)
+            return True
+        else:
+            sure_prev_tag = trellis[-2].keys()[0]
+            probability, prev_tag_at_state = (trellis[-2][sure_prev_tag][1], sure_prev_tag)
+            trellis[-1][word] = (prev_tag_at_state, 0)
+            return True
 
     return False
 
 
+def handle_one_backward_is_known(trellis,word):
+    key_pre_word = trellis[-2].keys()
+    assert len(key_pre_word) == 1
+    prev_tag = key_pre_word[0]
+    for j,tag in enumerate(tags_to_go_over):
+        prev_set_score = ( trellis[-2][prev_tag][1] + getScore(word, tag, prev_tag, trellis[-2][prev_tag][0]))
+        trellis[-1][tag] = (prev_tag, prev_set_score)
+
+
 def viterbi_decoder(words,prev_prev_tag,prev_tag):
     trellis = [{}]
+    knwon_one_backwords = False
     if len(words) > 0:
-        for tag in all_states:
-            prob = getScore(words[0], tag, prev_tag, prev_prev_tag)
-            trellis[0][tag] = (prev_tag,prob)
+        if is_special_case(trellis,words[0],0,words,prev_tag,prev_prev_tag):
+            knwon_one_backwords = True
+        else:
+            for tag in all_states:
+                prob = getScore(words[0], tag, prev_tag, prev_prev_tag)
+                trellis[0][tag] = (prev_tag,prob)
 
+    if len(words) == 1:
+        return trellis
 
-    if len(words) > 1:
-        trellis.append({})
-        for tag in all_states:
-            for i,prev_tag in enumerate(all_states):
-                score = getScore(words[1], tag, prev_tag, prev_prev_tag)
-                if i == 0:
-                    trellis[1][tag] = trellis[0][prev_tag][1] + score
-                elif trellis[0][prev_tag][1] + score > trellis[1][tag]:
-                    trellis[1][tag] = trellis[0][prev_tag][1] + score
-
-            # probability, prev_tag_at_state = max( [( trellis[0][prev_tag][1] + getScore(words[1], tag, prev_tag, prev_prev_tag), prev_tag) for prev_tag in all_states])
-            # trellis[1][tag] = (prev_tag_at_state,probability)
-
-    for i,word in enumerate(words[2:]):
+    for i,word in enumerate(words[1:]):
         trellis.append({})
         special_case = is_special_case(trellis,word,i, words, prev_tag,prev_prev_tag)
-        print "i is %0d"%i
-        if i == 27 and prev_tag_at_state == ',':
-            print "i is %0d" % i
-        if (not special_case):
-            for tag in all_states:
-                for prev_prev_tag in all_states:
-                    for j,prev_tag in enumerate(all_states):
-                        if j == 0:
-                            prev_set = prev_tag
-                            prev_set_score = (trellis[i+1][prev_tag][1] + getScore(word, tag, prev_tag, trellis[i+1][prev_tag][0]))
-                        elif (trellis[i+1][prev_tag][1] + getScore(word, tag, prev_tag, trellis[i+1][prev_tag][0]) >  prev_set_score):
-                            prev_set = prev_tag
-                            prev_set_score = (trellis[i+1][prev_tag][1] + getScore(word, tag, prev_tag, trellis[i+1][prev_tag][0]))
+        #print "i is %0d word is %s"%(i,word)
+        if special_case:
+            knwon_one_backwords = True
+            continue
+        if (not knwon_one_backwords):
+            for tag in tags_to_go_over:
+                option = []
+                for j,prev_tag in enumerate(tags_to_go_over):
+                    if j == 0:
+                        prev_set = prev_tag
+                        prev_set_score = (trellis[-2][prev_tag][1] + getScore(word, tag, prev_tag, trellis[-2][prev_tag][0]))
+                    elif (trellis[-2][prev_tag][1] + getScore(word, tag, prev_tag, trellis[-2][prev_tag][0]) >  prev_set_score):
+                        prev_set = prev_tag
+                        prev_set_score = (trellis[-2][prev_tag][1] + getScore(word, tag, prev_tag, trellis[-2][prev_tag][0]))
 
-                    probability, prev_tag_at_state = prev_set_score, prev_set
-                    trellis[-1][tag] = (prev_tag_at_state,probability)
-
+                probability, prev_tag_at_state = prev_set_score, prev_set
+                trellis[-1][tag] = (prev_tag_at_state,probability)
+        else:
+            handle_one_backward_is_known(trellis,word)
+            knwon_one_backwords = False
 
     return trellis
 
@@ -221,9 +239,9 @@ def find_answer(input_file_name,output_file_name):
     text = []
     f = open(input_file_name)
     for i,line in enumerate(f):
-        print "line number %0d"%i
-        if i==15:
-            print
+        #print "line number %0d"%i
+        if i%50 == 1:
+            print "line number %0d"%i
         words = line.strip().split(" ")
         prev_prev_tag = "START"
         prev_tag = "START"
@@ -240,6 +258,9 @@ def evaulate_result(output_file_name):
         text.append(line.strip())
     good = 0.0
     bad  = 0.0
+    wrong_tags= []
+    true_tags = []
+
     from MLETrain import extract_pos
     for i,correct_line in enumerate(open("ass1-tagger-test")):
         correct = extract_pos(correct_line)
@@ -249,6 +270,11 @@ def evaulate_result(output_file_name):
         for j in range(len(my_pos)):
             if (correct[j] == my_pos[j] ):
                 how_many_corrct += 1
+            else:
+                if (correct[j] == ")" or correct[j] == "("):
+                    print
+                wrong_tags.append(my_pos[j])
+                true_tags.append(correct[j])
         good += how_many_corrct
         bad += len(my_pos) - how_many_corrct
 
@@ -260,6 +286,7 @@ def evaulate_result(output_file_name):
     print ("good/(bad+good)")
     print (good/(bad+good))
 
+    print (zip(true_tags,wrong_tags))
 
 
 def main():
@@ -270,10 +297,10 @@ def main():
     output_file_name = sys.argv[4] if len(sys.argv) > 4 else "outpot.txt"
     extra_file_name  = sys.argv[5] if len(sys.argv) > 5 else "extra.txt"
 
-    create_transition(q_mle_filename)
-    create_emission(e_mle_filename)
-    create_denominator()
-    find_answer(input_file_name,output_file_name)
+    # create_transition(q_mle_filename)
+    # create_emission(e_mle_filename)
+    # create_denominator()
+    # find_answer(input_file_name,output_file_name)
     print ("Evaluate")
     evaulate_result(output_file_name)
 
